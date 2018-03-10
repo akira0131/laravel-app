c67372ba5feba82f221c1260bc1ca8125dedb348
akira0131 <bluesky.ar131@gmail.com>
2018-03-10 21:38:13 +0900

    クエリビルダ作成

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+//Route::get('home', 'HomeController@index')->name('home');
+Route::get('hello', 'HelloController@index');
+Route::get('person', 'PersonController@index');
+
+Route::get('hello/add', 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
+
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
+
+Route::get('hello/show', 'HelloController@show');
+
+Route::get('person/find', 'PersonController@find');
+Route::post('person/find', 'PersonController@search');
